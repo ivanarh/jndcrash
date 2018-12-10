9234873b3c0d5e1ac782806beb2033bd4a7965d6
Ivan Ponomarev <ivantrue@gmail.com>
2018-12-10 12:42:36 +0300

    Switched back to START_STICKY return value for NDCrashService.onStartCommand. Added a workaround with shared preferences to avoid parameters loss on service restart.

--- src/main/java/ru/ivanarh/jndcrash/NDCrashService.java
+++ src/main/java/ru/ivanarh/jndcrash/NDCrashService.java
@@ -2,6 +2,7 @@ package ru.ivanarh.jndcrash;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.annotation.CallSuper;
 import android.util.Log;
@@ -22,6 +23,11 @@ public class NDCrashService extends Service implements NDCrash.OnCrashCallback
     private static boolean mDaemonStarted = false;
 
     /**
+     * A name for shared preferences.
+     */
+    private static final String PREFS_NAME = "NDCrashService";
+
+    /**
      * Key for report file in arguments.
      */
     public static final String EXTRA_REPORT_FILE = "report_file";
@@ -33,21 +39,51 @@ public class NDCrashService extends Service implements NDCrash.OnCrashCallback
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (!mDaemonStarted && intent != null) {
-            mDaemonStarted = true;
-            final NDCrashUnwinder unwinder = NDCrashUnwinder.values()[intent.getIntExtra(EXTRA_UNWINDER, NDCrashUnwinder.libunwind.ordinal())];
-            final String reportPath = intent.getStringExtra(EXTRA_REPORT_FILE);
-            final NDCrashError initResult = NDCrash.startOutOfProcessDaemon(this, reportPath, unwinder, this);
-            if (initResult != NDCrashError.ok) {
-                Log.e(TAG, "Couldn't start NDCrash out-of-process daemon with unwinder: " + unwinder + ", error: " + initResult);
+        final SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        NDCrashUnwinder unwinder = null;
+        String reportPath = null;
+        if (intent != null) {
+            unwinder = NDCrashUnwinder.values()[intent.getIntExtra(EXTRA_UNWINDER, NDCrashUnwinder.libunwind.ordinal())];
+            reportPath = intent.getStringExtra(EXTRA_REPORT_FILE);
+            // Using the same keys as extras.
+            final SharedPreferences.Editor editor = preferences.edit();
+            if (unwinder != null) {
+                editor.putInt(EXTRA_UNWINDER, unwinder.ordinal());
             } else {
-                Log.i(TAG, "Out-of-process unwinding daemon is started with unwinder: " + unwinder + " report path: " +
-                        (reportPath != null ? reportPath : "null"));
-                onDaemonStart(unwinder, reportPath, initResult);
+                editor.remove(EXTRA_UNWINDER);
             }
+            if (reportPath != null) {
+                editor.putString(EXTRA_REPORT_FILE, reportPath);
+            } else {
+                editor.remove(EXTRA_REPORT_FILE);
+            }
+            editor.apply();
+        } else {
+            unwinder = NDCrashUnwinder.values()[preferences.getInt(EXTRA_UNWINDER, NDCrashUnwinder.libunwind.ordinal())];
+            reportPath = preferences.getString(EXTRA_REPORT_FILE, null);
         }
-        // We should preserve Intent from being lost when a process has been killed.
-        return Service.START_REDELIVER_INTENT;
+        if (!mDaemonStarted) {
+            if (unwinder != null) {
+                mDaemonStarted = true;
+                final NDCrashError initResult = NDCrash.startOutOfProcessDaemon(this, reportPath, unwinder, this);
+                if (initResult != NDCrashError.ok) {
+                    Log.e(TAG, "Couldn't start NDCrash out-of-process daemon with unwinder: " + unwinder + ", error: " + initResult);
+                } else {
+                    Log.i(TAG, "Out-of-process unwinding daemon is started with unwinder: " + unwinder + " report path: " +
+                            (reportPath != null ? reportPath : "null"));
+                    onDaemonStart(unwinder, reportPath, initResult);
+                }
+            } else {
+                Log.e(TAG, "Couldn't start NDCrash out-of-process daemon: unwinder is unknown.");
+            }
+        } else {
+            Log.i(TAG, "NDCrash out-of-process daemon is already started.");
+        }
+        // START_REDELIVER_INTENT may seem better but found by experimental way that when we return
+        // this value a service is restarted significantly slower (with a longer delay) after its
+        // process is killed. So a workaround is used: Saving initialization parameters to shared
+        // preferences and reading them when intent is null.
+        return Service.START_STICKY;
     }
 
     @Override @CallSuper
